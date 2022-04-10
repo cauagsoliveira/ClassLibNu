888dc744b318e44ad08b899bd026d58aa7bc09c8
Cauã Guedes <cauagsotto8@gmail.com>
2022-04-10 08:42:06 -0300

    classe itempedido

--- ClassLibNu/ItemPedido.cs
+++ ClassLibNu/ItemPedido.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace ClassLibNu
 {
-    internal class ItemPedido
+    
+    public class ItemPedido
     {
+        //atributos
+        private Produto produto;
+        private double valor;
+        private double quantidade;
+        private double desconto;
+
+        //propiedades
+        public Produto Produto { get { return produto; } set { produto = value; } }
+        public double Valor { get { return valor; } set { valor = value; } }
+        public double Quantidade { get { return quantidade; } set { quantidade = value; } }
+        public double Desconto { get { return desconto; } set { desconto = value; } }
+         
+        
+        // contrutores
+        public ItemPedido()
+        {
+        }
+
+        public ItemPedido(Produto produto, double valor, double quantidade, double desconto)
+        {
+            Produto = produto;
+            Valor = valor;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+        public void inserir() { }
+        public void change(ItemPedido item)
+        {
+
+        }
+     
+
+
+       
+
     }
 }
